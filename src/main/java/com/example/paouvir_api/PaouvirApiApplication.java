3bca317d533de1c2f6a0f06f48031c5bba94e70f
Brenno Ramos <brennoramos231@gmail.com>
2026-03-13 14:22:20 -0300

    primeiro commit

---
+++ src/main/java/com/example/paouvir_api/PaouvirApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.paouvir_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaouvirApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaouvirApiApplication.class, args);
+    }
+
+}
